c5f96718a3d84d839b57283425bc646f858570cd
Ömer Şerif <omerserif@Omer-MacBook-Air.local>
2026-03-25 12:04:09 +0300

    Locus yğk testleri yapıldı. Sonuçların ekran resimleri eklendi

--- locustfile.py
+++ locustfile.py
@@ -1,31 +1,36 @@
 from locust import HttpUser, task, between
 
 class LibraryUser(HttpUser):
-    # Kullanıcılar arası bekleme süresi (1-2 saniye)
     wait_time = between(1, 2)
     token = ""
 
     def on_start(self):
         """Her sanal kullanıcı test başında otomatik login olur"""
         payload = {
-            "username": "can",
-            "password": "123"
+            "username": "testuser",
+            "password": "test123"
         }
-        # Auth servisine giriş isteği atıyoruz
         with self.client.post("/auth/login", json=payload, catch_response=True) as response:
             if response.status_code == 200:
-                self.token = response.json().get("access_token")
+                self.token = response.json().get("token")
                 print("Başarıyla giriş yapıldı, token alındı.")
             else:
                 response.failure(f"Giriş Başarısız: {response.text}")
                 print(f"HATA: Giriş yapılamadı! Durum kodu: {response.status_code}")
 
     @task(3)
+    def list_books(self):
+        """Kitapları listeleme testi"""
+        if self.token:
+            headers = {"Authorization": f"Bearer {self.token}"}
+            self.client.get("/books", headers=headers)
+
+    @task(2)
     def check_borrows(self):
         """Ödünç alınan kitapları listeleme testi"""
         if self.token:
             headers = {"Authorization": f"Bearer {self.token}"}
-            self.client.get("/borrow/borrow", headers=headers)
+            self.client.get("/borrow", headers=headers)
 
     @task(1)
     def create_borrow(self):
@@ -37,4 +42,4 @@ class LibraryUser(HttpUser):
                 "book_id": 101,
                 "days": 14
             }
-            self.client.post("/borrow/borrow", json=payload, headers=headers)
+            self.client.post("/borrow", json=payload, headers=headers)
